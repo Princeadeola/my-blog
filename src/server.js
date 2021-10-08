8abfa524bed73059e6eb9652dbc09f3920e794f2
A-Adeola <56828114+Princeadeola@users.noreply.github.com>
2021-10-07 22:05:49 -0700

    First commit

---
+++ src/server.js
@@ -0,0 +1,75 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { MongoClient } from 'mongodb';
+import path from 'path'
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '/build')));
+app.use(bodyParser.json());
+
+// This function contains all connection to the mongodb
+const withDB = async (operations, res) => {
+    try {
+        const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
+        const db = client.db('my-blog');
+    
+        await operations(db);
+    
+        client.close();
+    } catch (error) {
+        res.status(500).json({ message: 'Error connecting to db', error });
+    }
+}
+
+// This function uses the withDB function to connect to the database and retrieve the article details 
+// based on url parse like (http://localhost:8000/api/articles/learn-node) using GET Method
+app.get('/api/articles/:name', async (req, res) => {
+    withDB(async (db) => {
+        const articleName = req.params.name;
+
+        const articleInfo = await db.collection('articles').findOne({ name: articleName })
+        res.status(200).json(articleInfo);
+    }, res);
+})
+
+// This function uses the withDB function to connect to the database and upvote the article  
+// based on url parse like (http://localhost:8000/api/articles/learn-node/upvote) using POST Method
+app.post('/api/articles/:name/upvote', async (req, res) => {
+    withDB(async (db) => {
+        const articleName = req.params.name;
+    
+        const articleInfo = await db.collection('articles').findOne({ name: articleName });
+        await db.collection('articles').updateOne({ name: articleName }, {
+            '$set': {
+                upvotes: articleInfo.upvotes + 1,
+            },
+        });
+        const updatedArticleInfo = await db.collection('articles').findOne({ name: articleName });
+    
+        res.status(200).json(updatedArticleInfo);
+    }, res);
+});
+
+app.post('/api/articles/:name/add-comment', (req, res) => {
+    const { username, text } = req.body;
+    const articleName = req.params.name;
+
+    withDB(async (db) => {
+        const articleInfo = await db.collection('articles').findOne({ name: articleName });
+        await db.collection('articles').updateOne({ name: articleName }, {
+            '$set': {
+                comments: articleInfo.comments.concat({ username, text }),
+            },
+        });
+        const updatedArticleInfo = await db.collection('articles').findOne({ name: articleName });
+
+        res.status(200).json(updatedArticleInfo);
+    }, res);
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/build/index.html'));
+});
+
+app.listen(8000, () => console.log('Listening on port 8000'));
